5b4361455bbf7113f6327961f0d8f941b9b0f4b2
jefferson.marin <jefferson.marin@correo.usa.edu.co>
2020-05-29 20:27:58 -0500

    add new end point

--- Server/model_big_data/viewsets.py
+++ Server/model_big_data/viewsets.py
@@ -11,48 +11,29 @@ import boto3
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
-from pyspark.sql.window import Window
-from pyspark.ml.feature import Tokenizer, RegexTokenizer
-from pyspark.ml.feature import StopWordsRemover
-from pyspark.ml.feature import HashingTF, IDF
-from pyspark.ml.feature import IDFModel
-from pyspark.ml import PipelineModel
+
+import re
+from joblib import dump, load
+
+def processing_text(texto):
+    # Remove all the special characters
+    processed_feature = re.sub(r'\W', ' ', str(texto))
+    # remove all single characters
+    processed_feature= re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_feature)
+    # Remove single characters from the start
+    processed_feature = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_feature) 
+    # Removing prefixed 'b'
+    processed_feature = re.sub(r'^b\s+', '', processed_feature)
+    # Converting to Lowercase
+    return processed_feature
+
 @api_view(['GET', 'POST'])
 def new_evento(request):
     if request.method == 'GET':
-        nombre_data= request.GET['data']
-        print(nombre_data)
-        newText=nombre_data
-        spark =  SparkSession.builder.appName("Taller").getOrCreate()
-        sentenceDataFrame = spark.createDataFrame([
-            (0, newText),
-        ], ["label", "text"])
-        tokenizer = Tokenizer(inputCol="text", outputCol="words")
-        regexTokenizer = RegexTokenizer(inputCol="text", outputCol="words", pattern="\\W")
-            # alternatively, pattern="\\w+", gaps(False)
-        regexTokenized2 = regexTokenizer.transform(sentenceDataFrame)
-
-        remover2 = StopWordsRemover(inputCol="words", outputCol="filtered")
-        remover2=remover2.transform(regexTokenized2)
-
-        hashingTF = HashingTF(inputCol="filtered", outputCol="rawFeatures", numFeatures=2**16)
-        featurizedData2 = hashingTF.transform(remover2)
-        idfModel2=IDFModel.load('/home/jefferson/Big Data/Corte3/idfmodel')
-
-        rescaledData2 = idfModel2.transform(featurizedData2)
-        sameModel = PipelineModel.load('/home/jefferson/Big Data/Corte3/sentiments_modelLG')
-        predictions2 = sameModel.transform(rescaledData2)
-        result_data=predictions2.select("prediction", "indexedLabel").collect()
-        prediction=result_data[0][0]
-        result_text=''
-        if prediction==0.0:
-            result_text='positive'
-        elif prediction==2.0:
-            result_text='negative'
-        else:
-            result_text='neutral'
-        print(result_text)
-
-        return Response({'validate':result_text}, status=status.HTTP_201_CREATED)
+        text= request.GET['data']
+        tfidfquery = load('tfidfquery') 
+        text_procesing=processing_text(text).lower()
+        features2 = tfidfquery.transform([text_procesing])
+        clf= load('semmtiments_analisys.joblib') 
+        response=clf.predict(features2)
+        return Response({'result':response}, status=status.HTTP_201_CREATED)
